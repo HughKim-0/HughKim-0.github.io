1aa2f8e4a9f9f2ae40df694d3004f633f64e5da4
Hugh Kim <wooni05@gmail.com>
2024-01-23 15:15:01 -0500

    commit

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -31,9 +31,9 @@ const RESOURCES = {"assets/AssetManifest.bin": "b45bdf91479216b87765a7a4ba1825b8
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "67d40a7af19de47409d0a8879b164869",
-"/": "67d40a7af19de47409d0a8879b164869",
-"main.dart.js": "8558da9c1a4e754a24ca247862816e35",
+"index.html": "8093f64c40ce2709e2283eb1e0b84dca",
+"/": "8093f64c40ce2709e2283eb1e0b84dca",
+"main.dart.js": "1aba44cf172491b2a585205000dda8fd",
 "manifest.json": "e74af8957b5899dc6da961caee768ec9",
 "version.json": "009c9e65172e010890f7f65fde438006"};
 // The application shell files that are downloaded before a service worker can
